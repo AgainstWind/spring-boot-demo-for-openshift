bb7db38936a139e4982705fb883fe885f455a750
yuezht <75134974@qq.com>
2018-02-07 16:24:36 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootdemoforopenshift/SpringBootDemoForOpenshiftApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootdemoforopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoForOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoForOpenshiftApplication.class, args);
+	}
+}
